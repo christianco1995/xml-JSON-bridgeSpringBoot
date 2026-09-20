b9628f9d593971e3be6fd56e29f041d664bf1bdc
Christianco1995 <christian.cervera.1995@gmail.com>
2026-09-20 04:57:37 +0200

    small fix in variable name

--- src/main/java/jcco/xml/json/bridge/GlobalExceptionHandler.java
+++ src/main/java/jcco/xml/json/bridge/GlobalExceptionHandler.java
@@ -23,11 +23,11 @@ public class GlobalExceptionHandler {
 	// For validations errors
 	@ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidations(MethodArgumentNotValidException ex) {
-        Map<String, String> errores = new HashMap<>();
+        Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> 
-            errores.put(error.getField(), error.getDefaultMessage())
+            errors.put(error.getField(), error.getDefaultMessage())
         );
-        return ResponseEntity.badRequest().body(errores);
+        return ResponseEntity.badRequest().body(errors);
     }
 	
 	// For format errors
